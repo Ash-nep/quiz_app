becdaa35cad8a74af8941a7aefdae258d3ccc39f
Ash-nep <asharma.it.nepal@gmail.com>
2026-08-04 22:48:44 +0545

    adding transparency to widgets

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -6,16 +6,22 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(context) {
     return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-        Opacity(
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+          Image.asset(
+          'assets/images/quiz-logo.png', 
+          width: 300,
+          color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+       
+        /*Opacity(
           opacity: 0.6,
           child: Image.asset(
           'assets/images/quiz-logo.png', 
           width: 300,
           ),
-        ),
+        ),*/
         const SizedBox(height: 80),
         const Text('Learn Flutter the fun way!',
           style: TextStyle(
@@ -33,7 +39,8 @@ class StartScreen extends StatelessWidget {
           label: const Text('Start Quiz'),
         )
 
-      ],),
+      ],
+    ),
     );
   }
 }
