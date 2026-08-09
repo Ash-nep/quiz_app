374acb7ec0a57c34f2e17131ee095b15ecae8013
Ash-nep <asharma.it.nepal@gmail.com>
2026-08-09 15:38:41 +0545

    configuring a column

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,6 +12,38 @@ const QuestionsScreen({super.key});
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('QuestionsScreen'); 
+    return SizedBox(
+      width: double.infinity, 
+      child:  Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text('The question...'),
+        const SizedBox(height: 30),
+        ElevatedButton(
+          onPressed: () {}, 
+          child: Text('Answer 1'),
+        ),
+        ElevatedButton(
+          onPressed: () {}, 
+          child: Text('Answer 2'),
+        ),
+        ElevatedButton(
+          onPressed: () {}, 
+          child: Text('Answer 3'),
+        ),
+        ElevatedButton(
+          onPressed: () {}, 
+          child: Text('Answer 4'),
+        ),
+        ElevatedButton(
+          onPressed: () {}, 
+          child: Text('Answer 5'),
+        ),
+        ElevatedButton(
+          onPressed: () {}, 
+          child: Text('Answer 6'),
+        ),
+      ],
+    ),); 
   }
 }
