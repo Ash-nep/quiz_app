5263c140d3a0be1d96a116f581376164813d90b0
Ash-nep <asharma.it.nepal@gmail.com>
2026-08-10 21:39:12 +0545

    added more conditions

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/questions_screen.dart';
-
+import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
@@ -12,7 +12,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-final List<String> selectedAnswers = [];
+List<String> selectedAnswers = [];
 var activeScreen = 'start-screen';
 
 void switchScreen(){
@@ -22,7 +22,14 @@ void switchScreen(){
 }
 
 void chooseAnswer(String answer) {
-  selectedAnswers.add(answer);
+  selectedAnswers.add(answer); 
+
+  if (selectedAnswers.length == questions.length) {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = 'start-screen';
+    });
+  }
 }
 
   @override
